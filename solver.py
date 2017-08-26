365066e182c30c99bdd7db064fa94fac01b6d465
Ahmed Abdulkadir <aoa13@outlook.com>
2017-08-26 08:35:38 -0400

    added grid function

--- solver.py
+++ solver.py
@@ -3,4 +3,13 @@ Ahmed Abdulkadir, 26 August 2017
 Sudoku Solver
 """
 
-a = 1
+def initializeGrid():
+    """
+    initalizes the 9x9 playing grid with zeros
+    """
+    grid = []
+    for row in range(9):
+        grid.append([])
+        for column in range(9):
+            grid[row].append(0)
+    return grid
